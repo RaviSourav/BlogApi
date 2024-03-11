568f08d8172792dce6e0e844faaf96b3b2ae0cc6
Ravi Sourav <rsourav@rsourav-ltmehx6.internal.salesforce.com>
2024-03-11 18:58:25 +0530

    Category First Part Added

--- src/main/java/com/springboot/blob/config/SecurityConfig.java
+++ src/main/java/com/springboot/blob/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authorize) ->
                         authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
-//                                .requestMatchers(HttpMethod.GET, "/api/categories/**").permitAll()
+//                                .requestMatchers(HttpMethod.POST, "/api/categories/**").permitAll()
                                 .requestMatchers("/api/auth/**").permitAll()
 //                                .requestMatchers("/swagger-ui/**").permitAll()
 //                                .requestMatchers("/v3/api-docs/**").permitAll()
